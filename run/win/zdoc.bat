0ba89bf3bdad224243c583ef4b03391f6e1bcf3e
zozoh <zozohtnt@gmail.com>
2009-07-08 05:34:57 +0000

    git-svn-id: http://nutzdoc.googlecode.com/svn/trunk@14 33619738-647a-11de-a80c-077126f7f4b5

---
+++ run/win/zdoc.bat
@@ -0,0 +1,9 @@
+@echo off
+SET JAVA_HOME=%JAVA_HOME%
+SET NUTZ_HOME=C:\eclipse\workspaces\zozoh\nutz\bin
+SET ZDOC_HOME=C:\eclipse\workspaces\zozoh\nutz.doc\bin
+
+SET CLASSPATH=.;%JAVA_HOME%\lib\tools.jar;%JAVA_HOME%\lib\dt.jar;%JAVA_HOME%\jre\rt.jar
+SET CLASSPATH=%CLASSPATH%;%ZDOC_HOME%;%NUTZ_HOME%;
+java org.nutz.doc.ZDoc %1 %2
+@echo on
