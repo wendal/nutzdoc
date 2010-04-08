a8b3a985f789663c5d3da4dfd391af79055099ed
zozoh <zozohtnt@gmail.com>
2010-04-08 10:25:55 +0000

    git-svn-id: http://nutzdoc.googlecode.com/svn/trunk@88 33619738-647a-11de-a80c-077126f7f4b5

--- build/nutzdoc-build.bat
+++ build/nutzdoc-build.bat
@@ -1,8 +1,9 @@
 @echo off
 SET JAVA_HOME=D:\LANG\Java\JDK1.6
-SET DEPS=D:\zzh\nutzam-deps
+SET DEPS=D:\zzh\depen-jars
 SET OUTPUT=D:\zzh\output
-SET PROJECT_HOME=D:\nutz\nutzam@google\trunk
+SET NUTZ=D:\workspace\galaxy\nutz@google\bin
+SET PROJECT_HOME=D:\nutz\nutzdoc@google\trunk
 
 D:
 cd %PROJECT_HOME%\build
